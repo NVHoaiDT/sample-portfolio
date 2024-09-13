25bf4ea19e63db1ff5f72242f144368acd01cace
NVHoaiDT <nguyenvanhoai1020@gmail.com>
2024-09-13 17:18:30 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## About this repo
+
+A demo portfolio site created just for the Web Programming subject, serving as an exam submission.
+
+## Warning
+
+This project originally used some legacy React patterns that have changed significantly in recent versions. I chose this template because it was easy to use and quick to deploy (Have to admit, I was pretty lazy 😅).
+
+Since this project primarily serves as an exam submission, I implemented some parts of the code in a "hacky" and somewhat messy way, which results in a lack of performance optimization, maintainability and scalability 😅. For these reasons, **I strongly don't recommend** using this entire repo as a foundation for a real-world website.
+
+
+## Requriment
+
+This project built which react, so you will need `node.js` installed globally on your machine.
+
+## Installation and Setup
+
+1. Installation: `npm install`
+
+2. In the project directory, you can run: `npm start`
+
+
+
---
+++ src/components/Home/Home2.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatars1.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiFillFacebook
+} from "react-icons/ai";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              ABOUT <span className="gradient"> ME </span> 
+            </h1>
+            <p className="home-about-body">
+              Just a web development enthusiast... 
+              <br />              
+              <br />
+              
+              I am passionate about developing products using <b className="">Node.js</b> and
+              <i>
+                <b className="">
+                  {" "}
+                  Modern Javascript Frameworks
+                </b>
+              </i>
+              &nbsp; such as
+              <i>
+                <b className=""> React.js, Next.js and TypeSript.</b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/NVHoaiDT"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+
+              <li className="social-icons">
+                <a
+                  href="https://www.facebook.com/NVHoaiDT1020/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillFacebook />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
